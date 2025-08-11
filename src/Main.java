65c07ebb8cc7a631a07ac13595a0c297bf9636b1
Aidan Winter <aidan.winter@students.mq.edu.au>
2025-08-11 14:29:32 +1000

    task 3 done

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,22 @@ public class Main extends JFrame {
       window.run();
     }
 
-    class Canvas extends JPanel {
+class Canvas extends JPanel {
       public Canvas() {
         setPreferredSize(new Dimension(720, 720));
       }
 
       @Override
-      public void paint(Graphics g) {
-	g.setColor(java.awt.Color.BLACK);
-	g.drawRect(10, 10, 700, 700);
-      }
+    public void paint(Graphics g) {
+	    g.setColor(java.awt.Color.BLACK);
+	    g.drawRect(10, 10, 700, 700);
+            for(int i = 10;i<700;i+=35){
+                for(int j = 10;j<700;j+=35){
+                    g.drawRect(i,j,35,35);
+                }
+            }
     }
+}
 
     private Main() {
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
