ad5e5e2390dea6ee841e1051bb9b30aa4f50d131
JamN9 <james.nguyen9@students.mq.edu.au>
2025-08-18 14:54:24 +1000

    Solutions for Week 04

---
+++ Actor.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class Actor {
+    Color color;
+    Cell loc;
+    void paint(Graphics g){
+        g.setColor(color);
+        g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+        g.setColor(Color.GRAY);
+        g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    };
+}
