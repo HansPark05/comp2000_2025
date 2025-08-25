5c09b3dc31fdd162eae257dc7e191dd806a35337
JamN9 <james.nguyen9@students.mq.edu.au>
2025-08-25 14:33:51 +1000

    Solution for Task 11

--- src/Actor.java
+++ src/Actor.java
@@ -1,14 +1,19 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.List;
 
 public abstract class Actor {
   Color color;
   Cell loc;
+  List<Polygon> display;
 
   public void paint(Graphics g) {
+    for(Polygon p : display){
     g.setColor(color);
-    g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    g.fillPolygon(p);
     g.setColor(Color.GRAY);
-    g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    g.drawPolygon(p);
+    }
   }
 }
